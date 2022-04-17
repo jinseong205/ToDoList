08dc402cc6fbeefa2f41ed1c13e4325cfa4ea97b
jinseong <jinseong205@gmail.com>
2022-04-17 23:32:34 +0900

    ^^7

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'my_to_do_app/index.html')
+    
